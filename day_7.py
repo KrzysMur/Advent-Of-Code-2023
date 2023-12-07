7acb9c4b49691851842153ecb954bbf8128c7dbe
KrzysMur <murek.krzys@gmail.com>
2023-12-07 18:35:03 +0100

    Day 7 - part 2

--- day_7.py
+++ day_7.py
@@ -1,4 +1,5 @@
 CARD_VALUES = {"2": "0", "3": "1", "4": "2", "5": "3", "6": "4", "7": "5", "8": "6", "9": "7", "T": "8", "J": "9", "Q": "a", "K": "b", "A": "c"}
+CARD_VALUES = {"J": "0","2": "1", "3": "2", "4": "3", "5": "4", "6": "5", "7": "6", "8": "7", "9": "8", "T": "9",  "Q": "a", "K": "b", "A": "c"}  # uncomment for part 2
 
 class Hand:
     def __init__(self, hand):
@@ -11,6 +12,14 @@ class Hand:
 
     def get_shape(self):
         cards = {card: self.cards.count(card) for card in CARD_VALUES.keys()}
+
+        # comment this section for part 2
+        jokers = cards["J"]
+        cards["J"] = 0
+        max_card = max(cards, key=cards.get)
+        cards[max_card] += jokers
+        ##################################
+
         if 5 in cards.values():
             return "6"
         if 4 in cards.values():
@@ -33,7 +42,7 @@ with open("puzzle_inputs/day_7_input.txt", "r") as file:
 
 
 
-def part_1():
+def main():
     sorted_hands = sorted(hands, key=lambda x: x.strength)
     total_winnings = 0
     rank = 1
@@ -43,4 +52,4 @@ def part_1():
     print(total_winnings)
 
 
-part_1()
+main()
